dd88a180533a8010f068925d1e2bc52d7f694e2d
Goran Culum <goran.culum@endava.com>
2023-03-01 11:30:13 +0100

    project init
    adding initial project files

---
+++ src/main/java/com/example/aop_poc/AopPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.aop_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopPocApplication.class, args);
+	}
+
+}
